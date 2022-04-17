bc613cbc52b2d1ed3c476d3a1a3998637ee9a10a
GeorgeMichael0803 <michaege@sheridancollege.ca>
2022-04-17 13:18:27 -0400

    creating the methods and static variables

---
+++ application_class.py
@@ -0,0 +1,15 @@
+class Application:
+
+    def __init__():
+        pass
+
+
+    def createSensors():
+        pass
+
+    def convertToDictionary():
+        pass
+
+    def findPath():
+        pass
+
---
+++ wireless_network.py
@@ -0,0 +1,10 @@
+from turtle import onclick
+
+
+class WirelessNetworks:
+    ADHOC_MODE= 'on'
+    BRAND_NAME='Cisco Brand'
+
+    @staticmethod
+    def greetMessage():
+        pass
