42afa1b9d3cf322b87ea6c5b4a486c65ab8fb5fa
GeorgeMichael0803 <michaege@sheridancollege.ca>
2022-04-17 22:18:34 -0400

    Code is working for output 1 and 2

---
+++ wireless_network.py
@@ -0,0 +1,34 @@
+class WirelessNetworks:
+
+
+    BRAND_NAME = 'Cisco'
+
+    ADHOC_Mode = 'ON'
+   
+   
+    
+    def greetMessage(self):
+        print("Welcome to the company's IoT-Based Health System \nThese are sensors of " , self.BRAND_NAME  , " brand, and the Ad Hoc Mode is " , self.ADHOC_Mode , "\n")
+
+    def __init__(self):
+        self.id = ''
+        self.neighbours = 0
+        self.oxygenLevel = 0
+        self.distance = ()
+        self.temperature = 0.0
+
+    def setID(self, Id):
+        self.id = Id
+
+    def getID(self):
+       return self.id
+
+
+    def setOxygenLevel(self, o2Level):
+        self.oxygenLevel = o2Level
+
+    def setTemperature(self, temp):
+        self.temperature = temp
+
+    def setNeighbours(self, neighbours):
+        self.neighbours = neighbours
